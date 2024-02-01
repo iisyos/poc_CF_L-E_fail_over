d2de9af2efd1cc621f4f8035d3d6703b58ad7b81
iisyos <kosuke1216a@yahoo.co.jp>
2024-02-01 23:49:19 +0900

    *feat: 検証結果

--- lambda/index.js
+++ lambda/index.js
@@ -4,6 +4,7 @@ exports.handler = (event, context, callback) => {
   const request = event.Records[0].cf.request;
   const uid = request.headers.uid ?? "NoUid";
   console.log(`uid: ${JSON.stringify(uid)}`);
+  throw new Error("");
 
   callback(null, request);
 };
